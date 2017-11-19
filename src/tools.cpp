39ade3a68db4a19f7ac94101b48a9051b64fe747
Maxim Pechyonkin <fazzolini@gmail.com>
2017-11-20 01:01:05 +0800

    implement RMSE calculation for 2 vectors

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,45 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
+  TODO: [DONE]
     * Calculate the RMSE here.
   */
+
+  // NOTE: I reused code from EKF project for this part
+  // default value is all zeros
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
+
+    // difference between truth and estimation
+    VectorXd residual;
+    residual = estimations[i] - ground_truth[i];
+
+    //coefficient-wise multiplication
+    VectorXd squared_residual;
+    squared_residual = residual.array() * residual.array();
+
+    // accumulate (element-wise addition)
+    rmse += squared_residual;
+  }
+
+  //calculate the mean
+  rmse = rmse / estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
+
 }
